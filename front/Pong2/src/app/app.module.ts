c8f488659753238a947619a24402924031e3b562
Alexandre D <des.alexandre@free.fr>
2018-02-05 16:59:12 +0100

    Intégration Login et Register

--- front/Pong2/src/app/app.module.ts
+++ front/Pong2/src/app/app.module.ts
@@ -3,17 +3,50 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-
+import {fakeBackendProvider} from "./_helpers/fake-backend";
+import {JwtInterceptor} from "./_helpers/jwt.interceptor";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {UserService} from "./_services/user.service";
+import {AuthenticationService} from "./_services/authentication.service";
+import {AuthGuard} from "./_guards/auth.guard";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+import {AlertComponent} from "./_directives/alert.component";
+import {AlertService} from "./_services/alert.service";
+import {FormsModule} from "@angular/forms";
+import {routing} from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { InvitationComponent } from './invitation/invitation.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
   declarations: [
     AppComponent,
+    AlertComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    InvitationComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front/Pong2/src/app/app.routing.ts
@@ -0,0 +1,19 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+import { AuthGuard } from './_guards/index';
+import {HomeComponent} from "./home/home.component";
+import {InvitationComponent} from "./invitation/invitation.component";
+
+const appRoutes: Routes = [
+  { path: '', component: InvitationComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
