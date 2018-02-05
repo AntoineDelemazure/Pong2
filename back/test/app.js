d3a5bc49dd66faa1ef825faa44f3daf524298f04
Antoine Delemazure <antoine.delemazure9@etu.univ-lorraine.fr>
2018-02-05 15:52:05 +0100

    test de l'existence de la bdd

--- back/test/app.js
+++ back/test/app.js
@@ -1,6 +1,7 @@
-/*  
-    Ceci est un bon exemple de test unitaire avec mocha
-*/
+var mysql      = require('mysql');
+
+//Ceci est un bon exemple de test unitaire avec mocha
+
 // This is just for organisation and reporting
 describe('Our application', function() {
 
@@ -19,12 +20,35 @@ describe('Our application', function() {
     });
 });
 
-describe('Fonctionnalités de bases', function(){
-    it('devrait nous assurer qu\'on accède bien à aap.js', function(done){
-        if(ultimate_truth()){
-            done();
-        }else{
-            done(new Error("Manifestement je n'accède pas au fichier"))
-        }
+describe("Test de la bdd", function(){
+
+    let connection
+
+    before(function(done){
+        connection = mysql.createConnection({
+            host     : 'localhost',
+            user     : 'loluser',
+            password : 'secret'
+        });
+        done();
     })
-})
+
+    it('devrait se connecter à la bdd', function(done){
+        let result
+        connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+            if (err) throw err
+            result  = rows[0].solution
+        })
+        console.log("lol "+result)
+        if(result == 2){
+            done()
+        }else{
+            done(new Error("On a du mal à acceder à la bdd" + result))
+        }
+    })// fin test
+
+    after(function(done){
+        connection.end()
+        done()
+    })
+})// fin describe
