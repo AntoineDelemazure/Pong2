cdae25266c3161a4549874cdd17ef80143025170
Antoine Delemazure <antoine.delemazure9@etu.univ-lorraine.fr>
2018-02-05 16:18:16 +0100

    meilleur test

--- back/test/app.js
+++ back/test/app.js
@@ -1,7 +1,7 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
-//Ceci est un bon exemple de test unitaire avec mocha
-
+// Ceci est un bon exemple de test unitaire avec mocha
+// TODO: On le supprimera peut-être
 // This is just for organisation and reporting
 describe('Our application', function() {
 
@@ -27,8 +27,8 @@ describe("Test de la bdd", function(){
     before(function(done){
         connection = mysql.createConnection({
             host     : 'localhost',
-            user     : 'loluser',
-            password : 'secret'
+            user     : 'root',
+            password : ''
         });
         done();
     })
@@ -37,14 +37,13 @@ describe("Test de la bdd", function(){
         let result
         connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
             if (err) throw err
-            result  = rows[0].solution
-        })
-        console.log("lol "+result)
-        if(result == 2){
-            done()
-        }else{
-            done(new Error("On a du mal à acceder à la bdd" + result))
-        }
+            result  = rows[0].solution //result == 2
+            if(result == 2){
+                done()
+            }else{
+                done(new Error("On a du mal à acceder à la bdd" + result))
+            }
+        })        
     })// fin test
 
     after(function(done){
