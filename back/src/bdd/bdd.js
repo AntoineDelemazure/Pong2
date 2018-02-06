12c203099156ca910c54e7a254a28f21096c92c1
Jofrey Luc <luc11u@etu.univ-lorraine.fr>
2018-02-06 15:13:31 +0100

    changement ce module pour l'exec sql

--- back/src/bdd/bdd.js
+++ back/src/bdd/bdd.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql')
-const execsql = require('execsql')
+const execsql = require('exec-sql')
 
 let connection // il vaudra peut-être mieux déplacer ça dans le main ?
 let directory_name = "../resources/ping_db.sql"
@@ -16,27 +16,42 @@ exports.connect = function() {
 exports.init = function(){
     connection.connect(function(err) {
         // si ça plante, on la recrée. (Il possible que ce soit parfaitement crétin.)
+
         if (err){
+            // connection.query("CREATE DATABASE ping_db", function (err, result) {
+            //     if (err) throw err;
+            //     console.log("Database created");
+            // });
             create()
             console.log("Base de donnée réinitalisée")
         }
     })
+    create()
 }
 
 create = function(){
-    let dbConfig = {
-        host: 'localhost',
-        user: 'root',
-        password: ''
-    }
-    let sql = 'use ping_db;'
-    let sqlFile = directory_name
-    execsql.config(dbConfig)
-    .exec(sql)
-    .execFile(sqlFile, function(err, results){
-        console.log(results);
-    }).end();
-    console.log("La bdd existe")
+    // let dbConfig = {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: ''
+    // }
+    // let sql = 'use ping_db;'
+    // let sqlFile = directory_name
+    // execsql.config(dbConfig)
+    // .exec(sql)
+    // .execFile(sqlFile, function(err, results){
+    //     console.log(results);
+    // }).end();
+    execsql.connect({
+        'database' : 'ping_db',
+        'user' : 'root',
+        'password' : ''
+    })
+    execsql.executeFile(directory_name, function(err) {
+        if (err) throw err
+        console.log("La bdd existe")
+    })
+
 }
 
 exports.getPeoplebyID = function(id){
