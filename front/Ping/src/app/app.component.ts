477ec7cfa0fd25a73cb5a3c9fa8273ff61294d9b
Alexandre D <des.alexandre@free.fr>
2018-02-12 14:46:44 +0100

    suppression de consulterprofile component
    Barre de navigation dynamique

--- front/Ping/src/app/app.component.ts
+++ front/Ping/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {User} from "./_models/user";
+import {UserService} from "./_services/user.service";
+import {AuthenticationService} from "./_services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentUser: User;
+  users: User[] = [];
   title = 'app';
+
+  constructor(private userService: UserService,
+              private authentificationService: AuthenticationService,
+              private router:Router
+  ) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  onActivate(){
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+
+  private logout(){
+    this.authentificationService.logout();
+    this.router.navigate(["['/']"]);
+  }
+
 }
