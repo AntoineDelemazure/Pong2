d04e85114a01fbfae0145d5de9dc05b8a6c2b8de
LeWalibiMonochrome <novano75@outlook.fr>
2018-02-12 14:18:44 +0100

    POUR ALEW

--- front/Ping/src/app/profil/profil.component.ts
+++ front/Ping/src/app/profil/profil.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+import {AuthenticationService} from "../_services/authentication.service";
+import {Router} from "@angular/router";
+
+
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
@@ -7,9 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  users: User[] = [];
+
+  constructor(private userService: UserService,
+              private authentificationService: AuthenticationService,
+              private router:Router
+  ) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+
+  private loadUsers() {
+  }
+
+  private logout(){
+    this.authentificationService.logout();
+    this.router.navigate(["['/']"]);
   }
 
 }
