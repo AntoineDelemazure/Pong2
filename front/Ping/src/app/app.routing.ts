6edd5d82ab324d87b36fd0749f9b1d5cb5bc5a90
LeWalibiMonochrome <novano75@outlook.fr>
2018-03-13 16:26:31 +0100

    Ajouter tournois

--- front/Ping/src/app/app.routing.ts
+++ front/Ping/src/app/app.routing.ts
@@ -8,6 +8,7 @@ import {InvitationComponent} from "./invitation/invitation.component";
 import {ProfilComponent} from "./profil/profil.component";
 import {TournamentListComponent} from "./tournament-list/tournament-list.component";
 import {TournamentDetailComponent} from "./tournament-detail/tournament-detail.component";
+import {TournamentRegisterComponent} from "./tournament-register/tournament-register.component";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'profil', component: ProfilComponent},
   { path: 'tournament', component: TournamentDetailComponent},
+  { path: 'tournament-register', component: TournamentRegisterComponent},
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
--- front/Ping/src/app/tournament-register/tournament-register.component.ts
+++ front/Ping/src/app/tournament-register/tournament-register.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TournamentRegisterComponent implements OnInit {
 
+
+  tournamentName:any;
+  tournamentDate:any;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register() {
+
   }
 
 }
