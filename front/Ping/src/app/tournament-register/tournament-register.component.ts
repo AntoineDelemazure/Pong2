10019067f0158df2ec63f2b55364b8fe00fd0ea0
LeWalibiMonochrome <novano75@outlook.fr>
2018-03-13 15:43:50 +0100

    Ajouter tournois

---
+++ front/Ping/src/app/tournament-register/tournament-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-register',
+  templateUrl: './tournament-register.component.html',
+  styleUrls: ['./tournament-register.component.css']
+})
+export class TournamentRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
